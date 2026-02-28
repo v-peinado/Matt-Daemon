8b3403250283383e0993615e2f230ce400c35f08
v-peinado <peinado1985@gmail.com>
2026-02-28 19:58:23 +0100

    FIX: unique is best for catch init errors

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,11 @@
 
 int main()
 {
+    std::unique_ptr<TintinReporter> logger;
     try
     {
-        TintinReporter logger;
-        MattDaemon daemon(logger);
+        logger = std::make_unique<TintinReporter>();
+        MattDaemon daemon(*logger);
         daemon.init();
         daemon.run();
         return 0;
@@ -17,6 +18,11 @@ int main()
     catch (const std::exception& e)
     {
         std::cerr << "Fatal error: " << e.what() << '\n';
+        if (logger && logger->isOpen())
+        {
+            logger->log(TintinReporter::LogLevel::Error, e.what());
+            logger->log(TintinReporter::LogLevel::Info, "Quitting.");
+        }
         return 1;
     }
 }
