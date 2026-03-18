fa3e81c5db230f45625e23599f48917f2da60300
v-peinado <peinado1985@gmail.com>
2026-03-18 20:29:59 +0100

    ADD: (beta) add struct and functions

--- client/includes/ArgParser.hpp
+++ client/includes/ArgParser.hpp
@@ -1,6 +1,14 @@
 #pragma once
+#include <iostream>
+#include <string>
 
 namespace ArgParser
 {
-    
+    struct parseStruct   // en c++ struct ya es un tipo no hace falta definirlo, y deja inicialiczarlo directamnte
+    {
+        std::string host = "127.0.0.1";
+        int port = 4242;
+    };
+    parseStruct parse(int argc, char** argv);
+    void printHelp();
 }
