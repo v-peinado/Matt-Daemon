1f5da9b8ba09d572f842db7a9f81e311d79eb03f
v-peinado <peinado1985@gmail.com>
2026-03-21 22:51:11 +0100

    UPd: add connection mebers

--- client/includes/Connection.hpp
+++ client/includes/Connection.hpp
@@ -4,10 +4,22 @@
 
 class Connection
 {
-
     public:
 
+        Connection(const std::string& host, int port);
+        Connection() = delete;
+
+        Connection(const Connection&) = delete;
+        Connection& operator=(const Connection&) = delete;
+        Connection(Connection&&) = delete;
+        Connection& operator=(Connection&&) = delete;
+        ~Connection();
 
     private:
+
+        std::string     m_host;
+        int             m_port;
+        int             m_socket;
+        bool            m_connected;
 
 };
